5e488dadfe0d17c6f72da45a6fca2ee4f085e09f
jimmyye1118 <jimmyye836@gmail.com>
2025-04-08 19:30:46 +0800

    重新訓練模型img_size:640，檔名為Test640

---
+++ Cube_Color.v3i.yolov11/README.dataset.txt
@@ -0,0 +1,6 @@
+# Cube Color > 2025-04-08 3:42pm
+https://universe.roboflow.com/jun-dvqkr/cube-color-gzmh4
+
+Provided by a Roboflow user
+License: CC BY 4.0
+
--- uploadRoboflow.py
+++ uploadRoboflow.py
@@ -3,6 +3,6 @@ import roboflow
 rf=roboflow.Roboflow(api_key="48TrqHbazjT2kcISiJpD")
 
 project = rf.workspace().project("cube-color-gzmh4")
-version = project.version(2)
+version = project.version(3)
 
-version.deploy("yolov11", "./model", "best.pt")
+version.deploy("yolov11", "./runs/detect/cube_training26/weights", "best.pt")
